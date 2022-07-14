5cfd76fd6018a9202b8116cdb6338ff10d96c4e9
Carlos && Ponce <srtager555@gmail.com>
2022-07-13 21:42:46 -0600

    added the folder of components

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,7 @@
+export function HomeHeader() {
+    return (
+        <div>
+            <p>Este es el home</p>
+        </div>
+    )
+}
---
+++ src/components/productsFilter.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+
+export function ProductsFilter({ productsList }) {
+   const [products, setProducts] = useState(productsList);
+   const [filterTitle, setFilterTitle] = useState("Filtro");
+   const [emptyFilter, setEmptyFilter] = useState(true);
+
+   function MenuFilter({ filterTitle }) {
+      return (
+         <>
+            <div>
+               <span>{filterTitle}</span>
+
+               <div></div>
+            </div>
+            <div>
+               {productsList.map((element, index) => {
+                  if (element.marca === "") return;
+
+                  return (
+                     <button
+                        key={`${element} - ${index}`}
+                        onClick={() => searchProduct(element.marca)}
+                     >
+                        {element.marca}
+                     </button>
+                  );
+               })}
+               {!emptyFilter ? <button onClick={clearFilter}>Limpiar filtro</button> : <></>}
+            </div>
+         </>
+      );
+   }
+
+   function clearFilter() {
+      setEmptyFilter(true);
+      setFilterTitle("Filtro")
+      setProducts(productsList);
+   }
+
+   function searchProduct(product) {
+      setEmptyFilter(false);
+      setFilterTitle(product);
+      setProducts(productsList.filter((element) => element.marca === product));
+   }
+
+   return (
+      <>
+         <MenuFilter filterTitle={filterTitle} />
+         <div>
+            {products.map((element) => {
+               return (
+                  <>
+                     {emptyFilter ? <h2>{element.marca}</h2> : <></>}
+                     {element.products.map((prd, index) => {
+                        return (
+                           <>
+                              <div key={`${element} - ${index}`}>
+                                 <p>
+                                    <span>{prd.name}</span>{" "}
+                                    <span>{prd.peso}</span>
+                                 </p>
+                                 {/* <p>{prd.marca}</p> */}
+                              </div>
+                           </>
+                        );
+                     })}
+                  </>
+               );
+            })}
+         </div>
+      </>
+   );
+}

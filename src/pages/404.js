8949c3569b06e3f441038b0339a97613357f5433
Carlos && Ponce <srtager555@gmail.com>
2022-07-14 01:14:30 -0600

    better JSX structure to 404 page]

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,16 @@
+import Link from "next/link"
+import Head from "next/head"
+
 export default function NotFoundPage() {
-    return "404 page not found"
+    return (
+        <>
+            <Head>
+                <title>404: pagina no encontrada.</title>
+            </Head>
+            <p>404: pagina no encontrada</p>
+            <Link href="/">
+                <a>Regresar a la pagina principal</a>
+            </Link>
+        </>
+    )
 }

dac49819f54ff14d5e949c98c5a6afa1a428d990
Carlos && Ponce <srtager555@gmail.com>
2022-07-14 23:12:54 -0600

    I updated this component

--- src/components/productsFilter.js
+++ src/components/productsFilter.js
@@ -8,15 +8,23 @@ export function ProductsFilter({ productsList }) {
    const [emptyFilter, setEmptyFilter] = useState(true);
    const [openMenuFilter, setOpenMenuFilter] = useState("");
 
-   const norteno = useRef(null)
-   const sanAntonio = useRef(null)
-   const delicia = useRef(null)
-   const elPorteno = useRef(null)
-   const franks = useRef(null)
-   const polloRey = useRef(null)
-   const toledo = useRef(null)
+   const norteno = useRef(null);
+   const sanAntonio = useRef(null);
+   const delicia = useRef(null);
+   const elPorteno = useRef(null);
+   const franks = useRef(null);
+   const polloRey = useRef(null);
+   const toledo = useRef(null);
 
-   const refArray = [norteno, sanAntonio, delicia, elPorteno, franks, polloRey, toledo]
+   const refArray = [
+      norteno,
+      sanAntonio,
+      delicia,
+      elPorteno,
+      franks,
+      polloRey,
+      toledo,
+   ];
 
    function MenuFilter({ filterTitle }) {
       return (
@@ -66,16 +74,19 @@ export function ProductsFilter({ productsList }) {
    }
 
    function clearFilter() {
+      window.scrollTo(0, window.innerHeight);
       setEmptyFilter(true);
       setFilterTitle("Filtro");
       setProducts(productsList);
+
+      setTimeout(() => toggleMenu(), 100);
    }
 
    function searchProduct(product) {
       setEmptyFilter(false);
       setFilterTitle(product);
       setProducts(productsList.filter((element) => element.marca === product));
-      window.scrollTo(0, window.innerHeight)
+      window.scrollTo(0, window.innerHeight);
 
       setTimeout(() => toggleMenu(), 100);
    }
@@ -90,7 +101,7 @@ export function ProductsFilter({ productsList }) {
          <div className={styles["filter__content"]}>
             {products.map((element) => {
                return (
-                  <>
+                  <div className={styles["filter__contet--container"]}>
                      {emptyFilter ? <h2>{element.marca}</h2> : <></>}
                      {element.products.map((prd, index) => {
                         return (
@@ -100,18 +111,24 @@ export function ProductsFilter({ productsList }) {
                                  key={`${element} - ${index}`}
                               >
                                  <h3>
-                                    <span className={styles["filter__content-product_title"]}>{prd.name}</span>{" "}
+                                    <span
+                                       className={
+                                          styles[
+                                             "filter__content-product_title"
+                                          ]
+                                       }
+                                    >
+                                       {prd.name}{prd.peso != "" ? "/ " : ""}
+                                    </span>
+
                                     <span>{prd.peso}</span>
                                  </h3>
-                                 <img
-                                    src={prd.img}
-                                    alt={prd.name}
-                                 />
+                                 <img src={prd.img} alt={prd.name} />
                               </div>
                            </>
                         );
                      })}
-                  </>
+                  </div>
                );
             })}
          </div>

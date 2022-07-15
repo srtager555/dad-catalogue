c04aadd91067c1732bec1ae9a95aa026af7cb975
Carlos && Ponce <srtager555@gmail.com>
2022-07-14 20:18:11 -0600

    I added the classes for the styles in sass

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,10 +1,22 @@
+import style from "@sass/home.module.sass";
+
 export function HomeHeader() {
-    return (
-        <div>
-            <h1>The One</h1>
-            <p>Carnes y mas...</p>
-            // bg image
-            <div></div>
-        </div>
-    )
+   return (
+      <div className={style["container__Home"]}>
+         <div className={style["container__Home--simple__BG"]}></div>
+         <div className={style["container__Home_content"]}>
+            <div className={style["container__Home--Introduction"]}>
+               <h1 className={style["container__Home--Introduction-title"]}>
+                  The One
+               </h1>
+               <p
+                  className={style["container__Home--Introduction-description"]}
+               >
+                  Carnes y mas...
+               </p>
+            </div>
+            <p className={style["container__Home--catalogue"]}>Catálogo</p>
+         </div>
+      </div>
+   );
 }

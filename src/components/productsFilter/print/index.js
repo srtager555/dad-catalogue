16146f2d94d41c4057923f03b0a6ba7d809d9864
Carlos && Ponce <srtager555@gmail.com>
2022-08-28 19:43:34 -0600

    Sintaxis of the new "filter" added

--- src/components/productsFilter/print/index.js
+++ src/components/productsFilter/print/index.js
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "@sass/filterHome.module.sass";
 
-export function FilterWeb({ productsList }) {
-	const [products, setProducts] = useState(productsList);
-	const [emptyFilter, setEmptyFilter] = useState(true);
+export function FilterPrint({ productsList }) {
+	const [newProductsArray, setNewProductsArray] = useState([]);
+
+	const newStructureArray = productsList
+		.map((element) => {
+			return element.products;
+		})
+		.flat();
+
+	useEffect(() => {
+		let a = newStructureArray.map((prd, index) => {
+			let quesoConQueso = newStructureArray.slice(index, index + 2);
+
+			newStructureArray.splice(index + 1, 1);
+
+			return quesoConQueso;
+		});
+
+		setNewProductsArray(a);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<div className={styles["filter"]}>
 			<div className={styles["filter__content"]}>
-				{products.map((element, index) => {
+				{newProductsArray.map((element, index) => {
 					return (
 						<div
 							key={`${element.marca} - ${index}`}
 							className={styles["filter__contet--container"]}
 						>
-							{emptyFilter ? <h2>{element.marca}</h2> : <></>}
-							{element.products.map((prd, index) => {
+							<div className={styles["brand__name"]}>
+								<h2>The One</h2>
+								<span className={styles["brands__name--line"]}></span>
+							</div>
+							<span className={styles["border__line--down"]}></span>
+							{element.map((prd, index) => {
 								return (
 									<div
 										className={styles["filter__content--element"]}
@@ -30,6 +53,7 @@ export function FilterWeb({ productsList }) {
 
 											<span>{prd.peso}</span>
 										</h3>
+										{/* eslint-disable-next-line @next/next/no-img-element */}
 										<img src={prd.img} alt={prd.name} />
 									</div>
 								);

0636d73bb6523aaacfa84fda189f524e75317cd9
Carlos && Ponce <srtager555@gmail.com>
2022-09-02 17:19:26 -0600

    New sintaxis to the cover added

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,19 +1,37 @@
+import { useState, useEffect } from "react";
+
 import style from "@sass/home.module.sass";
 
 export function HomeHeader() {
+	const [printMode, setPrintMode] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener("beforeprint", () => setPrintMode(true));
+		window.addEventListener("afterprint", () => setPrintMode(false));
+
+		return function () {
+			window.removeEventListener("beforeprint", () => setPrintMode(true));
+			window.removeEventListener("afterprint", () => setPrintMode(false));
+		};
+	}, []);
+
 	return (
-		<div className={style["container__Home"]}>
-			<div className={style["container__Home--simple__BG"]}></div>
-			<div className={style["container__Home_content"]}>
-				<div className={style["container__Home--Introduction"]}>
-					<h1 className={style["container__Home--Introduction-title"]}>
-						The One
-					</h1>
-					<p className={style["container__Home--Introduction-description"]}>
-						Carnes y mas...
+		<div className={style["container"]}>
+			{!printMode ? (
+				<div className={style["simple__BG"]}></div>
+			) : (
+				<div className={style["simple__BG"]}>
+					<img src="/image/logo_temporal.png" alt="" />
+				</div>
+			)}
+			<div className={style["content"]}>
+				<div className={style["Introduction"]}>
+					<h1 className={style["Introduction-title"]}>The One</h1>
+					<p className={style["Introduction-description"]}>
+						Calidad, buenos precios y atencio<span>´</span>n personalizada.
 					</p>
 				</div>
-				<p className={style["container__Home--catalogue"]}>Catálogo</p>
+				<p className={style["catalogue"]}>Catálogo</p>
 			</div>
 		</div>
 	);
